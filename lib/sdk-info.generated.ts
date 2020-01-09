40e902d35da2d603cf46689b864b8d3385c09447
Enngage <ennspassion@gmail.com>
2020-01-09 13:46:30 +0100

    update-sdk-version

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '@kentico/kontent-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '0.3.8',
+    version: '0.3.9',
     name: '@kentico/kontent-management'
 };
