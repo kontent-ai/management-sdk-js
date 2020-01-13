b4b30c16480b775b94b09243a5df7107bae60165
Enngage <ennspassion@gmail.com>
2020-01-13 09:57:54 +0100

    udpate-sdk-info

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '@kentico/kontent-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '0.3.9',
+    version: '0.3.10',
     name: '@kentico/kontent-management'
 };
