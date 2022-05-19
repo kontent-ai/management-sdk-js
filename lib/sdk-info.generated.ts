562a526b347dd3ef64d15d92ef8f6210e7e91239
Richard Sustek <ennspassion@gmail.com>
2022-05-19 10:21:28 +0200

    release

--- lib/sdk-info.generated.ts
+++ lib/sdk-info.generated.ts
@@ -2,6 +2,6 @@
 import { ISDKInfo } from '@kentico/kontent-core';
 export const sdkInfo: ISDKInfo = {
     host: 'npmjs.com',
-    version: '2.5.0',
+    version: '2.6.0',
     name: '@kentico/kontent-management'
 };
