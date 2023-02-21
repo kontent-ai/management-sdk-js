9bb4a86b936771744479f3db6bcaaf95fa986f4c
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2023-02-21 14:00:43 -0600

    added space-models based on content-item-model

---
+++ lib/models/spaces/space-models.ts
@@ -0,0 +1,25 @@
+import { SpaceContracts } from 'lib/contracts/space-contracts';
+import { SharedModels } from '../shared/shared-models';
+
+export namespace SpaceModels {
+
+    export class ContentItem implements SharedModels.IBaseModel<SpaceContracts.ISpaceModelContract> {
+
+        public id!: string;
+        public name!: string;
+        public codename!: string;
+        public _raw!: SpaceContracts.ISpaceModelContract;
+
+        constructor(
+            data: {
+                id: string,
+                name: string,
+                codename: string,
+                _raw: SpaceContracts.ISpaceModelContract
+            }
+        ) {
+            Object.assign(this, data);
+        }
+    }
+}
+
