5e1973be0f4f3e36d53ebc332e2d873ffcfb67c7
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2023-02-22 11:19:40 -0600

    added space responses to response index

---
+++ lib/mappers/spaces-mapper.ts
@@ -0,0 +1,7 @@
+import { IResponse } from '@kontent-ai/core-sdk';
+
+import { SpaceContracts } from '../contracts';
+import { SpaceModels } from '../models';
+import { SpaceResponses } from '../responses';
+import { BaseMapper } from './base-mapper';
+
