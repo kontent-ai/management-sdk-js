38b13e529ca4d86a1c7947996ed87e8bfabbe157
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2023-03-02 13:58:48 -0600

    created add space test

---
+++ test/browser/spaces/add-space.spec.ts
@@ -0,0 +1,48 @@
+import { SpaceResponses } from '../../../lib';
+import * as responseJson from '../fake-responses/workflow/fake-add-workflow.json';
+import { cmLiveClient, getTestClientWithJson, testProjectId } from '../setup';
+
+
+describe('Add space', () => {
+    let response: SpaceResponses.AddSpaceResponse;
+    beforeAll(async () => {
+        response = await getTestClientWithJson(responseJson)
+            .addSpace()
+            .withData({
+                name: 'My workflow',
+                codename: "My_Workflow"
+            })
+            .toPromise();
+    });
+
+    it(`url should be correct`, () => {
+        const url = cmLiveClient
+            .addSpace()
+            .withData({} as any)
+            .getUrl();
+        expect(url).toEqual(`https://manage.kontent.ai/v2/projects/${testProjectId}/spaces`);
+    });
+
+    it(`response should be instance of AddSpaceResponse class`, () => {
+        expect(response).toEqual(jasmine.any(SpaceResponses.AddSpaceResponse));
+    });
+
+    it(`response should contain debug data`, () => {
+        expect(response.debug).toBeDefined();
+    });
+
+    it(`response should contain data`, () => {
+        expect(response.data).toBeDefined();
+    });
+
+    it('space properties should be mapped',() => {
+        const originalItem = responseJson;
+        const space = response.data;
+
+        expect (space.codename).toEqual(originalItem.codename);
+        expect (space.name).toEqual(originalItem.name);
+        expect (space.id).toBeDefined;
+
+
+    })
+})
