e85be22ba7d2ebb895ee0e1cc70fd90853ea543c
Nicholas Margaritondo <nick.margaritondo@outlook.com>
2024-01-08 09:23:19 -0600

    created new webhook contract, updated naming of legacy webhook contracts

--- lib/contracts/webhook-contracts.ts
+++ lib/contracts/webhook-contracts.ts
@@ -3,7 +3,7 @@ export namespace WebhookContracts {
     export interface IDeleteWebhookResponseContract {
     }
 
-    export interface IWebhookTransitionsToContract {
+    export interface ILegacyWebhookTransitionsToContract {
         id: string;
     }
 
@@ -11,19 +11,66 @@ export namespace WebhookContracts {
 
     export type WebhookManagementContentChangesOperations = 'archive' | 'create' | 'restore';
 
-    export interface IWebhookWorkflowStepChangesContract {
+    export type WebhookContentTypeActions = 'created' | 'changed' | 'deleted';
+
+    export type WebhookAssetActions = 'created' | 'changed' | 'metadata_changed' | 'deleted';
+
+    export type WebhookTaxonomyActions = 'created' | 'metadata_changed' | 'deleted' | 'term_created' | 'term_changed' | 'term_deleted'| 'terms_moved';
+
+    export type WebhookLanguageActions = 'created' | 'changed' | 'deleted';
+
+    export type WebhookContentItemActions = 'published' | 'unpublished' | 'created' | 'changed' | 'metadata_changed' | 'deleted' | 'workflow_step_changed';
+
+    export interface ILegacyWebhookWorkflowStepChangesContract {
         type: 'content_item_variant';
-        transitions_to: IWebhookTransitionsToContract[];
+        transitions_to: ILegacyWebhookTransitionsToContract[];
     }
 
-    export interface IWebhookManagementApiContentChangesContract {
+    export interface ILegacyWebhookManagementApiContentChangesContract {
         type: 'content_item_variant';
         operations: WebhookManagementContentChangesOperations[];
     }
 
-    export interface IWebhookDeliveryApiContentChangesContract {
+    export interface ILegacyWebhookDeliveryApiContentChangesContract {
         type: 'taxonomy' | 'content_item_variant';
         operations: WebhookWorkflowStepOperationContract[];
+    }
+
+    export interface IWebhookContentTypeContract {
+        enabled: boolean;
+        actions?: WebhookContentTypeActions[];
+    }
+
+    export interface IWebhookAssetContract {
+        enabled: boolean;
+        actions?: WebhookAssetActions[];
+    }
+
+    export interface IWebhookTaxonomyContract {
+        enabled: boolean;
+        actions?: WebhookTaxonomyActions[];
+    }
+
+    export interface IWebhookLanguageContract {
+        enabled: boolean;
+        actions?: WebhookLanguageActions[];
+    }
+
+    export interface IWebhookContentItemContract {
+        enabled: boolean;
+        actions: WebhookContentItemActions[];
+    }
+
+    export interface ILegacyWebhookContract {
+        id: string;
+        name: string;
+        secret: string;
+        url: string;
+        last_modified?: string;
+        triggers: {
+            delivery_api_content_changes: ILegacyWebhookDeliveryApiContentChangesContract[],
+            workflow_step_changes: ILegacyWebhookWorkflowStepChangesContract[],
+        };
     }
 
     export interface IWebhookContract {
@@ -32,17 +79,23 @@ export namespace WebhookContracts {
         secret: string;
         url: string;
         last_modified?: string;
-        triggers: {
-            delivery_api_content_changes: IWebhookDeliveryApiContentChangesContract[],
-            workflow_step_changes: IWebhookWorkflowStepChangesContract[],
-        };
+        delivery_triggers: {
+            slot: string;
+            events: string;
+            asset?: IWebhookAssetContract[];
+            content_type?: IWebhookContentTypeContract[];
+            taxonomy?: IWebhookTaxonomyContract[];
+            language?: IWebhookLanguageContract[];
+            content_item?: IWebhookContentItemContract[];
+
+        }
     }
 
-    export interface IGetWebhookContract extends IWebhookContract {
+    export interface IGetWebhookContract extends ILegacyWebhookContract {
     }
 
-    export interface IAddWebhookContract extends IWebhookContract {
+    export interface IAddWebhookContract extends ILegacyWebhookContract {
     }
 
-    export type IWebhookListContract = IWebhookContract[];
+    export type IWebhookListContract = ILegacyWebhookContract[];
 }
