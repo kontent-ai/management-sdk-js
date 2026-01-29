30e234858584398d9da8161b214e8bc5381de4f2
Richard Sustek <ennspassion@gmail.com>
2026-01-29 10:17:41 +0100

    Adds missing id & display_mode to custom app model

--- lib/contracts/custom-apps-contracts.ts
+++ lib/contracts/custom-apps-contracts.ts
@@ -2,15 +2,19 @@ import { SharedContracts } from './shared-contracts';
 
 export namespace CustomAppsContracts {
     export interface ICustomAppContract {
+        id: string;
         name: string;
         codename: string;
         source_url: string;
         config: string | null;
         allowed_roles?: SharedContracts.ICodenameIdReferenceContract[];
+        display_mode: CustomAppDisplayMode;
     }
 
     export interface ICustomAppsListResponseContract {
         custom_apps: ICustomAppContract[];
         pagination: SharedContracts.IPaginationModelContract;
     }
+
+    export type CustomAppDisplayMode = 'fullScreen' | 'dialog';
 }
--- lib/models/custom-apps/custom-app.models.ts
+++ lib/models/custom-apps/custom-app.models.ts
@@ -3,26 +3,32 @@ import { SharedModels } from '../shared/shared-models';
 
 export namespace CustomAppModels {
     export class CustomApp implements SharedModels.IBaseModel<CustomAppsContracts.ICustomAppContract> {
+        public id: string;
         public name: string;
         public codename: string;
         public source_url: string;
         public config: string | null;
         public allowed_roles?: SharedContracts.ICodenameIdReferenceContract[];
+        public display_mode: CustomAppsContracts.CustomAppDisplayMode
         public _raw!: CustomAppsContracts.ICustomAppContract;
 
         constructor(data: {
             name: string;
+            id: string;
             codename: string;
             source_url: string;
             config: string | null;
             allowed_roles?: SharedContracts.ICodenameIdReferenceContract[];
+            display_mode: CustomAppsContracts.CustomAppDisplayMode;
             _raw: CustomAppsContracts.ICustomAppContract;
         }) {
+            this.id = data.id;
             this.name = data.name;
             this.codename = data.codename;
             this._raw = data._raw;
             this.source_url = data.source_url;
             this.config = data.config;
+            this.display_mode = data.display_mode;
         }
     }
 
@@ -34,34 +40,35 @@ export namespace CustomAppModels {
         allowed_roles?: SharedContracts.ICodenameIdReferenceContract[];
     }
 
+
     export type ModifyCustomAppPropertyName = 'name' | 'source_url' | 'config' | 'allowed_roles';
 
     export type ModifyCustomAppOperation =
         | {
-              op: 'addInto';
-              value: SharedContracts.IReferenceObjectContract;
-              property_name: Extract<ModifyCustomAppPropertyName, 'allowed_roles'>;
-          }
+            op: 'addInto';
+            value: SharedContracts.IReferenceObjectContract;
+            property_name: Extract<ModifyCustomAppPropertyName, 'allowed_roles'>;
+        }
         | (
-              | {
-                    op: 'replace';
-                    value: SharedContracts.IReferenceObjectContract;
-                    property_name: Extract<ModifyCustomAppPropertyName, 'allowed_roles'>;
-                }
-              | {
-                    op: 'replace';
-                    value: string;
-                    property_name: Extract<ModifyCustomAppPropertyName, 'name' | 'source_url'>;
-                }
-              | {
-                    op: 'replace';
-                    value: string | null;
-                    property_name: Extract<ModifyCustomAppPropertyName, 'config'>;
-                }
-          )
+            | {
+                op: 'replace';
+                value: SharedContracts.IReferenceObjectContract;
+                property_name: Extract<ModifyCustomAppPropertyName, 'allowed_roles'>;
+            }
+            | {
+                op: 'replace';
+                value: string;
+                property_name: Extract<ModifyCustomAppPropertyName, 'name' | 'source_url'>;
+            }
+            | {
+                op: 'replace';
+                value: string | null;
+                property_name: Extract<ModifyCustomAppPropertyName, 'config'>;
+            }
+        )
         | {
-              op: 'remove';
-              value: SharedContracts.IReferenceObjectContract;
-              property_name: Extract<ModifyCustomAppPropertyName, 'allowed_roles'>;
-          };
+            op: 'remove';
+            value: SharedContracts.IReferenceObjectContract;
+            property_name: Extract<ModifyCustomAppPropertyName, 'allowed_roles'>;
+        };
 }
